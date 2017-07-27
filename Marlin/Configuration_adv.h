23d1d7439c0fafee19189472746ab14370e7eca4
Kyle Granat <kyle.granat@gmail.com>
2017-07-26 20:09:47 -0400

    adjust max values for carriages

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -304,7 +304,8 @@
   // Note: the first x-carriage is defined as the x-carriage which homes to the minimum endstop;
   // the second x-carriage always homes to the maximum endstop.
   #define X2_MIN_POS CONF_X2_MIN     // set minimum to ensure second x-carriage doesn't hit the parked first X-carriage
-  #define X2_MAX_POS CONF_X2_MAX - CONF_X_MIN    // set maximum to the distance between toolheads when both heads are homed
+  //#define X2_MAX_POS CONF_X2_MAX - CONF_X_MIN    // set maximum to the distance between toolheads when both heads are homed
+  #define X2_MAX_POS CONF_X2_MAX    // set maximum to the distance between toolheads when both heads are homed
   #define X2_HOME_DIR 1     // the second X-carriage always homes to the maximum endstop position
   #define X2_HOME_POS CONF_X2_MAX // default home position is the maximum carriage position
       // However: In this mode the HOTEND_OFFSET_X value for the second extruder provides a software
